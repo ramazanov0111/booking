5d4279939c47c46142efa674114309f10ec4ce73
Renat Ramazanov <renat.ramazanov.1997@gmail.com>
2025-06-18 15:21:41 +0300

    refactor

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -24,8 +24,8 @@ class CreateNewUser implements CreatesNewUsers
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'lastname' => ['required', 'string', 'max:255'],
-            'login' => ['required', 'string', 'max:255', 'unique:users'],
-            'email' => ['string', 'email', 'max:255', 'unique:users'],
+            'login' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
